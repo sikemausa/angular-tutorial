252e9e37cf2f586961b39dddaedef020e01a846c
Mike Sausa <mikejsausa@gmail.com>
2017-03-06 11:43:54 -0700

    added reusable component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'my-app',
@@ -10,15 +11,7 @@ import { Component } from '@angular/core';
                   [class.selected]="hero === selectedHero">
                  <span class="badge">{{hero.id}}</span> {{hero.name}}
                </li>
-             </ul>
-             <div *ngIf="selectedHero">
-               <h2>{{selectedHero.name}} details!</h2>
-               <div><label>id: </label>{{selectedHero.id}}</div>
-               <div>
-                  <label>name: </label>
-                  <input [(ngModel)]="selectedHero.name" placeholder="name">
-                </div>
-              </div>`,
+             </ul>`,
    styles: [`
      .selected {
        background-color: #CFD8DC !important;
@@ -77,11 +70,6 @@ export class AppComponent  {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
-
-export class Hero {
-  id: number;
-  name: string;
 }
 
 const HEROES: Hero[] = [
